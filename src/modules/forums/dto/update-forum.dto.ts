705aec069e4215b5dc90c799839ad883533a1d3a
thanhdat <dat.nt@hodfords.com>
2023-10-24 22:18:54 +0700

    chore: validate update forum dto

--- src/modules/forums/dto/update-forum.dto.ts
+++ src/modules/forums/dto/update-forum.dto.ts
@@ -1,6 +1,7 @@
+import { BadRequestException } from '@nestjs/common';
 import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
 import { ResourceStatus } from '@prisma/client';
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsEnum, IsOptional, ValidateIf } from 'class-validator';
 import { CreateForumDto } from './create-forum.dto';
 
 export class UpdateForumDto extends PartialType(
@@ -10,6 +11,17 @@ export class UpdateForumDto extends PartialType(
     enum: ResourceStatus,
   })
   @IsOptional()
+  @ValidateIf((dto, value) => {
+    {
+      if (value) {
+        if (dto.name || dto.type || dto.topicIds) {
+          throw new BadRequestException('The status must be sent alone');
+        }
+      }
+
+      return true;
+    }
+  })
   @IsEnum(ResourceStatus)
   status?: ResourceStatus;
 }
