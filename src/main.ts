6cb67cf026b8c90ac9ff358fc5835dbd786ba39b
namvietanh1902 <namvietanh2002@gmail.com>
2023-10-31 21:15:56 +0700

    chore: add api prefix

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,8 @@ async function bootstrap() {
     }),
   );
 
+  app.setGlobalPrefix('api/v1');
+
   const { httpAdapter } = app.get(HttpAdapterHost);
 
   app.useGlobalFilters(new GlobalExceptionFilter());
